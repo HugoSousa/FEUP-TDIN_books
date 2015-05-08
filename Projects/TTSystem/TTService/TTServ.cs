0c0960d3c620c3d6f3078e2886fb205098701c21
hugo_sousa <77hugosousa@gmail.com>
2015-05-08 11:34:55 +0100

    First commit. Based on the demo solution provided by teacher.

---
+++ Projects/TTSystem/TTService/TTServ.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TTService {
+  public class TTServ : ITTServ {
+    public int AddTicket(int author, string problem) {
+      int id = 0;
+
+      using (SqlConnection c = new SqlConnection(ConfigurationManager.ConnectionStrings["TTs"].ConnectionString)) {
+        try {
+          c.Open();
+          string sql = "insert into TTickets(Author, Problem, Answer, Status) values (" + author + ", '" + problem + "', '', 1)";
+          SqlCommand cmd = new SqlCommand(sql, c);
+          cmd.ExecuteNonQuery();
+          cmd.CommandText = "select max(Id) from TTickets";
+          id = (int) cmd.ExecuteScalar();
+        }
+        catch (SqlException) {
+        }
+        finally {
+          c.Close();
+        }
+      }
+      return id;
+    }
+
+    public DataTable GetTickets(int author) {
+      DataTable result = new DataTable("TTickets");
+      
+      using (SqlConnection c = new SqlConnection(ConfigurationManager.ConnectionStrings["TTs"].ConnectionString)) {
+        try {
+          c.Open();
+          string sql = "select Id, Problem, Status, Answer from TTickets where Author=" + author;
+          SqlCommand cmd = new SqlCommand(sql, c);
+          SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+          adapter.Fill(result);
+        }
+        catch (SqlException) {
+        }
+        finally {
+          c.Close();
+        }
+      }
+      return result;
+    }
+  }
+}
---
+++ WebSites/TTSite/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Web.UI;
+using TTSvc;
+
+public partial class _Default : Page {
+  TTServClient proxy;
+
+  protected void Page_Load(object sender, EventArgs e) {
+    proxy = new TTServClient();
+  }
+
+  protected void Button1_Click(object sender, EventArgs e) {
+    int id = 0;
+
+    if (DropDownList1.SelectedIndex > 0) {
+      if (TextBox1.Text.Length > 0) {
+        id = proxy.AddTicket(Convert.ToInt32(DropDownList1.SelectedValue), TextBox1.Text);
+        Label1.ForeColor = Color.DarkBlue;
+        Label1.Text = "Result: Inserted with Id = " + id;
+      }
+      else {
+        Label1.ForeColor = Color.Red;
+        Label1.Text = "Result: Please describe a problem!";
+      }
+    }
+    else {
+      Label1.ForeColor = Color.Red;
+      Label1.Text = "Result: Select an Author!";
+    }
+  }
+
+  protected void Button2_Click(object sender, EventArgs e) {
+    if (DropDownList1.SelectedIndex > 0) {
+      GridView1.DataSource = proxy.GetTickets(Convert.ToInt32(DropDownList1.SelectedValue));
+      GridView1.DataBind();
+      GridView1.Visible = true;
+      Label2.Text = "";
+    }
+    else {
+      GridView1.Visible = false;
+      Label2.Text = "Select an Author!";
+    }
+  }
+}
